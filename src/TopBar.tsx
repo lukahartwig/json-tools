f1eb60b2eb620266b57a10a47c29ba81ef2b8f56
Luka Hartwig <mail@lukahartwig.de>
2022-01-19 00:44:17 +0100

    Add GitHub link

--- src/TopBar.tsx
+++ src/TopBar.tsx
@@ -1,4 +1,5 @@
 import { Component } from "solid-js";
+import GitHubIcon from "./GitHubIcon";
 
 import styles from "./TopBar.module.css";
 
@@ -6,6 +7,13 @@ const TopBar: Component = () => {
   return (
     <header class={styles.topbar}>
       <h1 class={styles.title}>JSON Set</h1>
+      <a
+        class={styles.link}
+        href="https://github.com/lukahartwig/json-tools"
+        target="_blank"
+      >
+        <GitHubIcon />
+      </a>
     </header>
   );
 };
