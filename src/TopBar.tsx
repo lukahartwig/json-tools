8898924e6633175126f0226bb2d62a61b4b71f9d
Luka Hartwig <mail@lukahartwig.de>
2022-01-19 02:00:33 +0100

    Change highlight color

--- src/TopBar.tsx
+++ src/TopBar.tsx
@@ -6,7 +6,7 @@ import styles from "./TopBar.module.css";
 const TopBar: Component = () => {
   return (
     <header class={styles.topbar}>
-      <h1 class={styles.title}>JSON Set</h1>
+      <h1 class={styles.title}>JSON Tools</h1>
       <a
         class={styles.link}
         href="https://github.com/lukahartwig/json-tools"
